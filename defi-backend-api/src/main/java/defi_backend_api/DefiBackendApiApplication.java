946010f72e45f7051c0e0f477a4a922fff200baa
Vijay-948 <kvijayreddy948@gmail.com>
2024-06-18 15:29:49 +0530

    first-commit

---
+++ defi-backend-api/src/main/java/defi_backend_api/DefiBackendApiApplication.java
@@ -0,0 +1,13 @@
+package defi_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefiBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefiBackendApiApplication.class, args);
+	}
+
+}
---
+++ defi-backend-api/src/test/java/defi_backend_api/DefiBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package defi_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefiBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
