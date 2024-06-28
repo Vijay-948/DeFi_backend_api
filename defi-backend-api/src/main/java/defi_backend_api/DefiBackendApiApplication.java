0de378df30a8a704f1a386862508124340deecec
Vijay-948 <kvijayreddy948@gmail.com>
2024-06-28 21:43:53 +0530

    added-packages

--- defi-backend-api/src/main/java/defi_backend_api/DefiBackendApiApplication.java
+++ defi-backend-api/src/main/java/defi_backend_api/DefiBackendApiApplication.java
@@ -2,8 +2,10 @@ package defi_backend_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class DefiBackendApiApplication {
 
 	public static void main(String[] args) {
