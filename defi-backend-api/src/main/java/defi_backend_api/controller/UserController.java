7901b2d54d146055c6ca62d9da8cf73378fb4d5f
Vijay-948 <kvijayreddy948@gmail.com>
2024-07-23 00:12:45 +0530

    added-annotation

--- defi-backend-api/src/main/java/defi_backend_api/controller/UserController.java
+++ defi-backend-api/src/main/java/defi_backend_api/controller/UserController.java
@@ -1,4 +1,9 @@
 package defi_backend_api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class UserController {
 }
--- defi-backend-api/src/main/java/defi_backend_api/repository/OtpRepository.java
+++ defi-backend-api/src/main/java/defi_backend_api/repository/OtpRepository.java
@@ -2,9 +2,10 @@ package defi_backend_api.repository;
 
 import defi_backend_api.entity.OtpBO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface OtpRepository extends JpaRepository<OtpBO, String> {
 
     Optional<OtpBO> findByEmail(String email);
