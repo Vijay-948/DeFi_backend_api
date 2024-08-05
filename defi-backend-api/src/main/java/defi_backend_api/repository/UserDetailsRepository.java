c54a5c3312000a9e4b6f0f79150b14778b9add06
Vijay-948 <kvijayreddy948@gmail.com>
2024-08-05 23:13:27 +0530

    created-common-util

--- defi-backend-api/src/main/java/defi_backend_api/repository/UserDetailsRepository.java
+++ defi-backend-api/src/main/java/defi_backend_api/repository/UserDetailsRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserDetailsRepository extends JpaRepository<UserDetailsBO, Long> {
+public interface UserDetailsRepository extends JpaRepository<UserDetailsBO, Integer> {
 
     Optional<UserDetailsBO> findByEmail(String email);
 }
