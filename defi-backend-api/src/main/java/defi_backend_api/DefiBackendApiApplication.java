fee66b8fbb4b2915c1d8c18b82edc0053f20b4b0
Vijay-948 <kvijayreddy948@gmail.com>
2024-08-18 22:44:05 +0530

    added-annototation

--- defi-backend-api/src/main/java/defi_backend_api/DefiBackendApiApplication.java
+++ defi-backend-api/src/main/java/defi_backend_api/DefiBackendApiApplication.java
@@ -2,13 +2,17 @@ package defi_backend_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableCaching
+@EnableAsync
 public class DefiBackendApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DefiBackendApiApplication.class, args);
 	}
-
 }
